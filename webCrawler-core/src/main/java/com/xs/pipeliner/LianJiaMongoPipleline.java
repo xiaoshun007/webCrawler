82b706819cc703d8d71766e4bc9b1c6dc613236b
xiaoshun <495314431@qq.com>
2017-12-18 22:16:19 +0800

    持久化到mongodb逻辑修改：存在相同的数据就更新，否则新增

--- webCrawler-core/src/main/java/com/xs/pipeliner/LianJiaMongoPipleline.java
+++ webCrawler-core/src/main/java/com/xs/pipeliner/LianJiaMongoPipleline.java
@@ -1,8 +1,11 @@
 package com.xs.pipeliner;
 
 import com.alibaba.fastjson.JSON;
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.xs.configure.MongoDBConfiguration;
 import com.xs.data.Mongo;
@@ -21,6 +24,7 @@ public class LianJiaMongoPipleline implements Pipeline {
 
     @Override
     public void process(ResultItems resultItems, Task task) {
+
         String result = JSON.toJSONString(resultItems.getAll());
 
         MongoDBConfiguration mongoDBConfiguration = new MongoDBConfiguration();
@@ -32,14 +36,41 @@ public class LianJiaMongoPipleline implements Pipeline {
             // 连接到数据库
             MongoDatabase mongoDatabase = mongoClient.getDatabase(mongo.getDbName());
             MongoCollection<Document> collection = mongoDatabase.getCollection(mongo.getCollectionName());
-            Document document = Document.parse(result);
-            collection.insertOne(document);
+
+            // 记录存在则更新
+            if (isUrlExists(resultItems, collection)) {
+                Document newDocument = new Document();
+                newDocument.put("$set", Document.parse(result));
+
+                Document queryDoc = new Document();
+                queryDoc.put("url", resultItems.get("url"));
+
+                collection.updateOne(queryDoc, newDocument);
+            } else {
+                Document document = Document.parse(result);
+                collection.insertOne(document);
+            }
 
             // 关闭mongodb防止异常
             mongoClient.close();
         }catch(Exception e){
             logger.error( e.getClass().getName() + ": " + e.getMessage() );
         }
+    }
+
+    private boolean isUrlExists(ResultItems resultItems, MongoCollection<Document> collection) {
+        String curUrl = resultItems.get("url");
+
+        Document searchQuery = new Document();
+        searchQuery.put("url", curUrl);
+
+        FindIterable<Document> findIterable = collection.find(searchQuery);
+        MongoCursor<Document> mongoCursor = findIterable.iterator();
+        while(mongoCursor.hasNext()) {
+            logger.info(curUrl + "exists");
+            return true;
+        }
+        return false;
     }
 
     public static void main( String args[] ){
