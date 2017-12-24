9c16fd202c27df383e1b11dce3274309effa1f1a
xiaoshun <495314431@qq.com>
2017-12-24 20:58:41 +0800

    zookeeper demos

---
+++ webCrawler-data-analysis/src/main/java/com/xs/data/zookeeper/CuratorDemo.java
@@ -0,0 +1,25 @@
+package com.xs.data.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+
+public class CuratorDemo {
+    static String path = "/zk-book/c1";
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("localhost:2181")
+            .sessionTimeoutMs(5000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath(path, "init".getBytes());
+        System.out.println("node created: " + new String(client.getData().storingStatIn(new Stat()).forPath("/zk-book/c1")));
+    }
+}
