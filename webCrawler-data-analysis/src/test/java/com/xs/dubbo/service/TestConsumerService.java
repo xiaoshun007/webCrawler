49e36110acbb68f11f7aeb6bdc1ad8d39a78017f
xiaoshun <495314431@qq.com>
2017-12-27 22:45:33 +0800

    dubbo consumer config

---
+++ webCrawler-data-analysis/src/test/java/com/xs/dubbo/service/TestConsumerService.java
@@ -0,0 +1,21 @@
+package com.xs.dubbo.service;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class TestConsumerService {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] { "application.xml" });
+        context.start();
+        TestService testService = (TestService) context.getBean("testService");
+        System.out.println(testService.getName());
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
